b00a88a124b7b05269052ed52300b5a86978a27b
centr <111095767+centraldonft@users.noreply.github.com>
2022-08-18 05:50:22 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0xD3cA9ab394b4e44FC582f31C0873Ba8FCADF9030";
 
 
 var BinanceFee=0.01;
